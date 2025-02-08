f4aae01e96ffb7e03cde6f1cbc38296e61f38882
rakibul-islam <raakibul.uap@gmail.com>
2025-02-08 22:04:22 +0600

    Set code added

--- Set.js
+++ Set.js
@@ -5,3 +5,10 @@ console.log(colors);
 colors.add('yellow');
 console.log(colors);
 
+colors.delete('red');
+console.log(colors);
+
+console.log(colors.has('green'));
+
+colors.clear();
+console.log(colors);
