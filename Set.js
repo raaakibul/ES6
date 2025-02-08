4fdda9429338ee2a408dc60d78ca233ee5c7ab9c
rakibul-islam <raakibul.uap@gmail.com>
2025-02-08 22:06:36 +0600

    Set

--- Set.js
+++ Set.js
@@ -12,3 +12,16 @@ console.log(colors.has('green'));
 
 colors.clear();
 console.log(colors);
+
+var country = new Set();
+country.add('India');
+country.add('USA');
+country.add('UK');
+country.add('Australia');
+console.log(country);
+console.log(country.size);
+console.log(country.values());
+console.log(country.keys());
+console.log(country.entries());
+console.log(country.has('India'));
+console.log(country.delete('USA'));
